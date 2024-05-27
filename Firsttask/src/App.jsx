abcb0098f0463773cddd4b16b309f90f6b9c07c0
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-05-27 18:42:35 +0500

    reactfirst program

---
+++ Firsttask/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Profile from './Profile'
+
+function App() {
+  
+
+  return (
+    
+      
+          <div>
+            <Profile/>
+          </div>
+        )
+        
+      }
+    
+  
+
+export default App
---
+++ Firsttask/src/Profile.jsx
@@ -0,0 +1,19 @@
+function Profile() {
+return(
+    <div>
+    <div className="Heading" >
+    <h1>My Bio</h1>
+    </div>
+    <p> Amir khan</p>
+    <p>Recently Completed Bachelor In Urdu </p>
+    <p>Living In Lahore </p>
+    <p>Started Working as a tranee </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    <p>Started Worling as a tranee </p>
+    
+    </div>
+)
+}
+
+export default Profile;
+
